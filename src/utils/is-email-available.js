ccb1370b4efe707544615fe24e939b9d63e5a721
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-06-09 18:47:40 -0500

    Update is-email-available.js

--- src/utils/is-email-available.js
+++ src/utils/is-email-available.js
@@ -1,5 +1,5 @@
 import {noop} from '~/src/utils'
-var debug = require('debug')('riqra-service-users:is-email-available')
+var debug = require('debug')('service-users:is-email-available')
 var knex = require('~/src/initializers/knex')
 
 module.exports = async function (value) {
